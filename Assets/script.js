c12d243da19a47d9e75fd67f9629fc49b59201cf
William James <Williamj3795@gmail.com>
2022-09-24 15:07:43 -0400

    added event handlers

--- Assets/script.js
+++ Assets/script.js
@@ -4,6 +4,7 @@ var lastCitySearched = ""
 
 // api call to openweathermap.org
 var getCityWeather = function(city) {
+    
     // declare the OpenWeather api url
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ce39e7239416ad754359ca762d28521a&units=imperial";
      // request call to api
@@ -12,7 +13,7 @@ var getCityWeather = function(city) {
      // request was successful
          if (response.ok) {
              response.json().then(function(data) {
-                 displayWeather(data);
+                 showWeather(data);
              });
          // request fails
          } else {
@@ -25,3 +26,69 @@ var getCityWeather = function(city) {
          alert("Unable to connect to OpenWeather");
      })
 };
+console.log("api is called", getCityWeather);
+
+
+// function to handle search button
+var searchSubmitHandler = function(event) {
+    event.preventDefault();
+    
+    // get value from input element
+    var cityName = $("#city-name").val().trim();
+
+    // check if the search field has a value
+    if(cityName) {
+        // pass the value to getCityWeather function
+        getCityWeather(cityName);
+
+        // clear the search input
+        $("#city-name").val("");
+    }
+};
+console.log("value is passed", searchSubmitHandler);
+
+// function to show the information collected from openweathermap.org
+var showWeather = function(weatherData) {
+
+    // format and display the values
+    $("#main-city-name").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ").append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
+    $("#main-city-temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + "°F");
+    $("#main-city-humid").text("Humidity: " + weatherData.main.humidity + "%");
+    $("#main-city-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + " mph");
+
+       // use lat & lon to make the uv api call
+       fetch("https://api.openweathermap.org/data/2.5/uvi?lat=" + weatherData.coord.lat + "&lon="+ weatherData.coord.lon + "&appid=ce39e7239416ad754359ca762d28521a")
+       .then(function(response) {
+           response.json().then(function(data) {
+
+               // display the uv index value
+               $("#uv-box").text(data.value);
+
+               // highlight the value using the EPA's UV Index Scale colors
+               if(data.value >= 11) {
+                   $("#uv-box").css("background-color", "#6c49cb")
+               } else if (data.value < 11 && data.value >= 8) {
+                   $("#uv-box").css("background-color", "#d90011")
+               } else if (data.value < 8 && data.value >= 6) {
+                   $("#uv-box").css("background-color", "#f95901")
+               } else if (data.value < 6 && data.value >= 3) {
+                   $("#uv-box").css("background-color", "#f7e401")
+               } else {
+                   $("#uv-box").css("background-color", "#299501")
+               }      
+           })
+       });
+       console.log("this function works", showWeather)
+    };
+
+
+
+
+    // event handlers
+$("#search-form").submit(searchSubmitHandler);
+$("#search-history").on("click", function(event){
+    // get the links id value
+    var prevCity = $(event.target).closest("a").attr("id");
+    // pass it's id value to the getCityWeather function
+    getCityWeather(prevCity);
+});
